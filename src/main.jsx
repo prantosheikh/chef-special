2a2c0c7e9385b92533df6b4c30aef7631c79d77d
Pranto Sheikh <prantosheikh834@gmail.com>
2023-05-04 17:00:56 +0600

    Favorite Fecipe button added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import AuthProvider from "./provider/AuthProvider";
 import router from "./router/Rotutes";
--- src/pages/ChefAndRecipe/ChefAndRecipe.jsx
+++ src/pages/ChefAndRecipe/ChefAndRecipe.jsx
@@ -1,74 +1,103 @@
 import React, { useContext } from "react";
 import { Card, CardGroup, Container } from "react-bootstrap";
-import { FaHeart } from "react-icons/fa";
+import { FaHeart, FaStar, FaStarHalfAlt } from "react-icons/fa";
+import Rating from "react-rating";
 import { useLoaderData } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { AuthCondext } from "../../provider/AuthProvider";
 
 const ChefAndRecipe = () => {
   const chefRecipes = useLoaderData();
-  const {loading} = useContext(AuthCondext) 
+  const { loading } = useContext(AuthCondext);
 
   console.log(chefRecipes);
 
   return (
     <Container>
-      <div className="row">
-        <div className="col-8">
-          <Card className="border border-0">
-            <Card.Img variant="top" src={chefRecipes?.ChefPicture} />
+      <div className="my-5">
+        <div className="row">
+          <div className="col-8">
+            <Card className="border border-0">
+              <Card.Img variant="top" src={chefRecipes?.ChefPicture} />
 
-            <Card.Body>
-              <div>
-                <span className="fs-5 fw-semibold">
-                  {chefRecipes?.YearsOfExperience} Years
-                </span>
-                <span className="fs-5 ms-3 fw-semibold">
-                  {chefRecipes?.NumbersoFrecipes} Recipes
-                </span>
-
-                <FaHeart className="fs-5 ms-3"></FaHeart>
-              </div>
-
-              <Card.Title className="fs-1">{chefRecipes?.ChefName}</Card.Title>
-            </Card.Body>
-          </Card>
-        </div>
-        <div className="col-4">
-          {chefRecipes?.recipes?.slice(0, 1).map((recipe, index) => (
-            <Card className="border border-0" key={index}>
-              <Card.Img variant="top" src={recipe?.Image} />
               <Card.Body>
-                <p>{recipe?.RecipeName}</p>
-                <p>Ingredients : {recipe?.Ingredients}</p>
-                <p>{recipe?.Directions} </p>
+                <div>
+                  <span className="fs-5 fw-semibold">
+                    {chefRecipes?.YearsOfExperience} Years
+                  </span>
+                  <span className="fs-5 ms-3 fw-semibold">
+                    {chefRecipes?.NumbersoFrecipes} Recipes
+                  </span>
+
+                  <FaHeart className="fs-5 ms-3"></FaHeart>
+                </div>
+
+                <Card.Title className="fs-1">
+                  {chefRecipes?.ChefName}
+                </Card.Title>
               </Card.Body>
             </Card>
+          </div>
+          <div className="col-4">
+            {chefRecipes?.recipes?.slice(0, 1).map((recipe, index) => (
+              <Card className="border border-0" key={index}>
+                <Card.Img variant="top" src={recipe?.Image} />
+                <Card.Body>
+                  <p>{recipe?.RecipeName}</p>
+                  <p>Ingredients : {recipe?.Ingredients}</p>
+                  <p>{recipe?.Directions} </p>
+                </Card.Body>
+              </Card>
+            ))}
+            <div className="mt-5">CATEGORIES</div>
+          </div>
+        </div>
+        <Container className="d-flex justify-content-between ">
+          {chefRecipes?.recipes.map((recipe) => (
+            <CardGroup>
+              <Card style={{ width: "25rem" }} className="border border-0">
+                <Card.Img variant="top" src={recipe?.Image} />
+                <Card.Body>
+                  <h4 className="fw-bold">{recipe?.RecipeName}</h4>
+                  <p className="text-secondary-emphasis fs-5 ">
+                    Ingredients : {recipe?.Ingredients}
+                  </p>
+                  <p className="text-secondary">{recipe?.Directions}</p>
+                </Card.Body>
+                <Card.Footer>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="d-flex  gap-2 align-items-center">
+                      <Rating
+                        className="text-danger fs-5"
+                        placeholderRating={recipe?.Likes}
+                        emptySymbol={<FaStarHalfAlt></FaStarHalfAlt>}
+                        placeholderSymbol={<FaStar></FaStar>}
+                        fullSymbol={<FaStar></FaStar>}
+                        readonly
+                      />
+                      <span className="fs-5">{recipe.Likes}</span>
+                    </div>
+                    <div>
+                      <div>
+                        <button
+                          className="btn btn-danger"
+                          onClick={() => toast.success("Favorite Fecipe!")}
+                        >
+                          favorite
+                        </button>
+                        <ToastContainer />
+                      </div>
+                    </div>
+                  </div>
+                </Card.Footer>
+              </Card>
+            </CardGroup>
           ))}
-          <div className="mt-5">CATEGORIES</div>
-        </div>
+        </Container>
       </div>
-      <Container className="d-flex justify-content-between ">
-        {chefRecipes?.recipes.map((recipe) => (
-          <CardGroup>
-            <Card style={{ width: "25rem" }} className="border border-0">
-              <Card.Img variant="top" src={recipe?.Image} />
-              <Card.Body>
-                <h4 className="fw-bold">{recipe?.RecipeName}</h4>
-                <p className="text-secondary-emphasis fs-5 ">
-                  Ingredients : {recipe?.Ingredients}
-                </p>
-                <p className="text-secondary">{recipe?.Directions}</p>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-          </CardGroup>
-        ))}
-      </Container>
     </Container>
   );
 };
 
 export default ChefAndRecipe;
-
